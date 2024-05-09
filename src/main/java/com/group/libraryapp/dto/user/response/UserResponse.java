d8ae8473701bdb995e63d815cfc2f9daa2b652e1
thdgmlwns1 <xhrl233@naver.com>
2024-05-09 20:14:32 +0900

    3주차

--- src/main/java/com/group/libraryapp/dto/user/response/UserResponse.java
+++ src/main/java/com/group/libraryapp/dto/user/response/UserResponse.java
@@ -7,7 +7,13 @@ public class UserResponse {
     private String name;
     private Integer age;
 
-//    public UserResponse(long id, String name, Integer age) {
+    public UserResponse(long id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    //    public UserResponse(long id, String name, Integer age) {
 //        this.id = id;
 //        this.name = name;
 //        this.age = age;
